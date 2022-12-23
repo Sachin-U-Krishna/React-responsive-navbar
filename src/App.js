39e121f3d95b5f52fe739a862896655e2e68fd03
Sachin Krishna <liberty.sachin56@gmail.com>
2022-12-23 22:02:09 +0530

    Files added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Navbar from "./Components/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "./Components/Home";
+import About from "./Components/About";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import { NavLink} from 'react-router-dom'
+import './navbar.css'
+import { Icon } from '@iconify/react';
+
+export default function Navbar() {
+    const [icon, setIcon] = useState("icon-park:hamburger-button") 
+
+    let navLinkActive = {
+        borderBottom: "3px solid #2d4a8a",
+        color: "#2d4a8a"
+    };
+
+    function menuButton() {
+        if (!document.getElementById('menu').checked) {
+            document.getElementById('menu').checked = true;
+            document.getElementById('navbar').style.animation = "menuDown 250ms linear";
+            console.log()
+            window.setTimeout(() => {
+                document.getElementById('menuList').style.display = "block";
+                document.getElementById('navbar').style.animation = "none";
+                setIcon("mdi:close");
+            }, 240);
+            return;
+        }
+        document.getElementById('menu').checked = false;
+        window.setTimeout(() => {
+            document.getElementById('menuList').style.display = "none";
+            document.getElementById('menuList').style.animation = "none";
+            setIcon("icon-park:hamburger-button");
+        }, 75);
+        document.getElementById('menuList').style.animation = "menuUp 250ms ease-out";
+    }
+
+    window.onresize = () => {
+        if (window.innerWidth > 768) {
+            if (document.getElementById('menuList').style.display !== "inline-flex") {
+                document.getElementById('menuList').style.display = "inline-flex";
+                document.getElementById('menu').checked = false;
+                setIcon("icon-park:hamburger-button");
+                document.getElementById('menuList').style.animation = "none";
+            }
+            return;
+        }
+        if (!document.getElementById('menu').checked) {
+            document.getElementById('menuList').style.display = "none";
+        }
+    }
+
+    return (
+        <>
+            <div className="nav-header">
+                <nav id="navbar">
+                    <span id="nav-title"><NavLink className="nav-head" to="/">Navbar</NavLink></span>
+                    <ul id="menuList">
+                        <NavLink to="/" style={({ isActive }) => isActive ? navLinkActive : undefined}>
+                            <li>Home</li>
+                        </NavLink>
+                        <NavLink to="about" style={({ isActive }) => isActive ? navLinkActive : undefined}>
+                            <li>About</li>
+                        </NavLink>
+                        <NavLink to="/">
+                            <li>Sample</li>
+                        </NavLink>
+                        <NavLink to="/">
+                            <li>Contact</li>
+                        </NavLink>
+                    </ul>
+                    <span className="hamburger">
+                        <input type="checkbox" id="menu" />
+                        <Icon icon={icon} width="46" onClick={()=>menuButton()}/>
+                            {/* <iconify-icon icon="icon-park:hamburger-button" width="46" onclick="menuButton()"></iconify-icon> */}
+                    </span>
+                </nav>
+            </div>
+        </>
+    )
+}
